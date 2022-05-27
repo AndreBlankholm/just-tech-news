6bd258fd864134bc81d0d702cd6108996041c711
AndreBlankholm <ablankholm@gmail.com>
2022-05-27 12:38:14 -0500

    hi

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,12 @@
-//import the Sqeuelize construtor from the nod library
-
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create a (CALL) connection to my database, pass in your MySQL information for username and password
-
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD,  {
-    host: 'localhost',    //sql configure settings
-    dialect: 'mysql',
-    port: 3306
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
 });
-
 
 module.exports = sequelize;
